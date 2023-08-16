e5f405eb361a920df86c548b4e89d259e1d6869d
n0r7ex <136929702+n0r7ex@users.noreply.github.com>
2023-08-17 00:08:57 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector(".menu__btn");
 const menuMobile = document.querySelector(".header-menu__list");
-
 menuBtn.addEventListener("click", () => {
 	menuMobile.classList.toggle("menu--open");
 });
+
+
+
+
